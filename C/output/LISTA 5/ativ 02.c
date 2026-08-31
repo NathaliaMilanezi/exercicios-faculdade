b42883b7fef5f926e296b2dc08871eded1a6d11f
NathaliaMilanezi <nathaliamilanezi849@gmail.com>
2026-08-31 13:16:19 -0300

    Create ativ 02.c

---
+++ C/output/LISTA 5/ativ 02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h> // acentuação
+#include <math.h>
+#include <time.h> 
+
+ 
+
+//----------------PROGRAMA PRINCIPAL------------------//
+
+int main(){ 
+    SetConsoleOutputCP(65001); 
+    
+    
+    
+    return 0; 
+}
