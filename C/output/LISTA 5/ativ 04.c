18b5feb6770d57c4d676251e2143bcde52b17d15
NathaliaMilanezi <nathaliamilanezi849@gmail.com>
2026-09-02 09:35:06 -0300

    exercicio

--- C/output/LISTA 5/ativ 04.c
+++ C/output/LISTA 5/ativ 04.c
@@ -4,13 +4,41 @@
 #include <math.h>
 #include <time.h> 
 
+//----------------LER NÚMERO------------------//
+
+void lerNum(int *x, int *y){
+
+    printf("Digite o valor de X: ");
+    scanf("%d", x); 
+
+    printf("Digite o valor de y: "); 
+    scanf("%d", y); 
+}
+
+//----------------CALCULO------------------//
+
+void conta(int x, int y){
+
+    int cont, calc;
+    cont = 0;
+    calc = 0;  
+
+    while (cont < y){
+
+        calc = calc + x;
+        cont = cont + 1; 
+    }
+
+    printf("Resultado: %d", calc);  
+}
+
 //----------------PROGRAMA PRINCIPAL------------------//
 
 int main(){ 
     
     SetConsoleOutputCP(65001); 
     
-    float  x, y; 
+    int  x, y; 
 
     lerNum(&x, &y); 
     conta(x, y); 
